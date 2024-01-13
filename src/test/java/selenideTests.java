0301384e2aec8b031d906c3a67ac4310be467dc6
MoreInside <cyberbelka.msk@gmail.com>
2024-01-13 23:17:46 +0300

    add project

---
+++ src/test/java/selenideTests.java
@@ -0,0 +1,48 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
+
+public class selenideTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        //Открыть ресурс
+        open("https://github.com/");
+
+        //Нажать на строку поиска
+        $$(".flex-1").findBy(matchText("Search or jump to..."))
+                .shouldBe(enabled).click();
+
+        //Ввести в строку поиска слово "Selenide" и нажать Ввод
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+
+        //Открыть страницу Selenide в Github
+        $$("[data-testid='results-list']").first().$("a").click();
+
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-body").shouldHave(text("Soft assertions")).shouldBe(visible);
+
+        //Открыть страницу SoftAssertion; проверить, что внутри есть пример кода для JUnit5
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("@Listeners({ SoftAsserts.class})"));
+
+    }
+}
