7a5e7e28aefeae4ca5acd13c480b410b191b2c1b
MoreInside <cyberbelka.msk@gmail.com>
2024-01-14 17:13:47 +0300

    added corrected class

--- src/test/java/SelenideTests.java
+++ src/test/java/SelenideTests.java
@@ -12,7 +12,6 @@ public class SelenideTests {
     @BeforeAll
      static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
