528eec35d79c0c26b269e80710963a07cf6962e9
MoreInside <cyberbelka.msk@gmail.com>
2024-02-04 18:44:47 +0300

    added SelenideTests2 HW part 2

--- src/test/java/SelenideTests2.java
+++ src/test/java/SelenideTests2.java
@@ -1,11 +1,12 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTests2 {
     @BeforeAll
@@ -18,16 +19,37 @@ public class SelenideTests2 {
     @Test
     void selenideHomework_2 (){
 
-        //Открыть ресурс
         open("https://github.com/");
 
-        //выберите меню Solutions -> Enterprize с помощью команды hover для Solutions
         $(".header-menu-wrapper").$(byText("Solutions")).hover();
         $(".header-menu-wrapper").$(byText("Enterprise")).click();
 
-        //Убедитесь что загрузилась нужная страница (например что заголовок - "The AI-powered"
         $(".application-main").shouldHave(text("The AI-powered"));
 
 
     }
-}
+
+    @Test
+    void selenideHomework_2_1 () {
+
+        //Запрограммируйте Drag&Drop с помощью Selenide.actions()
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDrop(to($("#column-b")));
+
+        //Проверьте, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+    }
+
+    @Test
+    void selenideHomework_2_2() {
+
+            open("https://the-internet.herokuapp.com/drag_and_drop");
+            actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+            $("#column-a").shouldHave(text("B"));
+            $("#column-b").shouldHave(text("A"));
+        }
+
+    }
