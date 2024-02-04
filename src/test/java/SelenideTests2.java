70cb14c55920e620dca0e07ed608d692fb7e3e30
MoreInside <cyberbelka.msk@gmail.com>
2024-02-04 18:45:42 +0300

    added SelenideTests2 HW part 2_2

--- src/test/java/SelenideTests2.java
+++ src/test/java/SelenideTests2.java
@@ -13,7 +13,7 @@ public class SelenideTests2 {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
     @Test
